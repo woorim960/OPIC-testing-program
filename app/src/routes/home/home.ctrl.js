1de2cc7da9fd67ee70197faa108f96504d1ba67b
박우림 <woorimprog@gmail.com>
2021-07-05 19:00:11 +0900

    Fix: db 모듈 불러오지 않도록 변경

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const db = require("../../config/db");
+// const db = require("../../config/db");
 
 const view = {
   home: (req, res) => {
     res.render("home/index");
   },
 };
+
 module.exports = { view };
